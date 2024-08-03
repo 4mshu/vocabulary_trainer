dd41d9f04cc2ab134658581018643b7b64612b76
4mshu <2hungrygators@gmail.com>
2024-08-03 14:22:34 +0545

    word card and hints logic added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 
+import { useState } from 'react';
 import './App.css';
+import Hints from './components/Hints';
 import WordCard from './components/WordCard';
+import { words } from './components/words';
+
+let length = words.length;
+const randomNumber = Math.floor(Math.random() * (length));
+let rightWord = words[randomNumber].word
+let definition = words[randomNumber].definition
+let synonyms = words[randomNumber].synonyms
+
+
+
 
 function App() {
+  const [hints, setHints] = useState(false)
+  
+  function handleHints () {
+    setHints(hint=>!hint);
+  }
+
+
   return (
     <>
-      <WordCard/>
+      <WordCard definition={definition} handleHints={handleHints}/>
+      {hints && <Hints synonyms={synonyms} />}
+      
     </>
 
   );
--- src/components/WordCard.jsx
+++ src/components/WordCard.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import { words } from './words'
-
-let length = words.length;
-const randomNumber = Math.floor(Math.random() * (length + 1));
-let rightWord = words[randomNumber].word
-let definition = words[randomNumber].definition
+import { words } from './words.js'
 
 
-export default function WordCard() {
+export default function WordCard(props) {
+  const {definition, handleHints} = props;
   return (
-    <div>
-        <p>{definition}</p>
-    </div>
+    <fieldset className='absolute top-1/4 left-1/2 transform -translate-x-1/2 capitalize bg-gradient-to-r from-slategray to-lightblue text-white p-5 text-center rounded-lg text-xl font-bold max-w-xl shadow-lg border-2 border-white'>
+      <legend className='text-right'>
+        <button onClick={handleHints}>
+          <i className="fa-regular fa-lightbulb"></i>
+        </button>
+      </legend>
+    {definition}
+  </fieldset>
   )
+
 }
