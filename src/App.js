11f65ad09e3f0431804fc6a295d14c00ab10407d
4mshu <2hungrygators@gmail.com>
2024-08-04 15:00:09 +0545

    search feature added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import './App.css';
 import Hints from './components/Hints';
 import WordCard from './components/WordCard';
 import { words } from './components/words';
-import Options from './components/Options';
 import Sentences from './components/Sentences';
 import Buttons from './components/Buttons';
-import HeaderMessage from './components/HeaderMessage';
+const apiKey = process.env.REACT_APP_API_KEY;
 
 let length = words.length;
 const randomNumber = Math.floor(Math.random() * (length));
-let rightWord = words[randomNumber].word
 let definition = words[randomNumber].definition
 let synonyms = words[randomNumber].synonyms
 let sentences = words[randomNumber].sentences
@@ -21,6 +19,7 @@ let sentences = words[randomNumber].sentences
 function App() {
   const [hints, setHints] = useState(false)
   const [sentenceToggle, setSentenceToggle] = useState(false)
+  const [inputValue, setInputValue] = useState("")
   
   function handleHints () {
     setHints(hint=>!hint);
@@ -30,11 +29,28 @@ function App() {
     setSentenceToggle(sentenceToggle => !sentenceToggle)
   }
 
+  const handleClick = () => {
+    vocalBuilder(inputValue);
+  };
+
+  function vocalBuilder (word) {
+    console.log(word);
+  }
+
 
   return (
     <>
-      <HeaderMessage/>
-      <Options rightWord={rightWord} />
+      <div className='text-center p-4'>
+      <input
+        className='border-4 border-blue-500 text-3xl font-bold h-16 w-64 rounded-lg shadow-lg z-50 mx-auto p-2 bg-slate-300 text-blue-400'
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={handleClick} className='border-4 border-blue-500 text-2xl font-semibold h-16 w-24 rounded-lg shadow-lg z-50 mx-20 p-2 bg-slate-300 text-blue-400'>
+        Search
+      </button>
+    </div>
       <WordCard definition={definition}/>
       <Buttons handleHints={handleHints} handleSentenceToggle={handleSentenceToggle} />
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
